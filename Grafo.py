4270ec6cfbc247b69d02f245b7356e2706e62b81
b <b>
2019-05-15 00:14:46 -0300

    Mudancas no grafo

--- Grafo.py
+++ Grafo.py
@@ -11,12 +11,9 @@ class Grafo:
 
 	# Construtor da classe Grafo
 	# Parameto vertices deve ser uma lista de objetos da classe Vertice
-	def __init__(self, vertices):
-		for v in vertices:
-			if v.nome not in self.grafo:
-				self.grafo[v.nome] = v
-			else:
-				raise ValueError("Ha um vertice duplicado no grafo, por favor, mude o nome deste vertice: " + v.nome)
+	def __init__(self):
+		self.arestas = []
+		pass
 
 	# Retorna o grau de entrada de um vertice
 	# Parametro vertice: Eh o vertice que se deseja saber o grau de entrada
@@ -24,16 +21,6 @@ class Grafo:
 		entrada = 0
 		for v in self.grafo:
 			if vertice in self.adjacentes(v):
-				entrada += 1
-		return entrada
-
-	# Retorna o grau de entrada de um vertice de um grafo dado
-	# Parametro vertice: Eh o vertice que se deseja saber o grau de entrada
-	# Parametro graph: Eh o grafo que sera utilizado para a busca
-	def grauEntradaG(self, graph, vertice):
-		entrada = 0
-		for v in graph:
-			if vertice in self.adjacentesG(graph, v):
 				entrada += 1
 		return entrada
 
@@ -73,9 +60,9 @@ class Grafo:
 	def conecta(self, v1, v2):
 		if (v1 not in self.grafo) or (v2 not in self.grafo):
 			return False
-
-		if v1 not in self.grafo[v2].arestas: self.grafo[v2].arestas.append(v1)
-		if v2 not in self.grafo[v1].arestas: self.grafo[v1].arestas.append(v2)
+		
+		if not self.grafo[v1].arestaExiste(v2): self.arestas(self.grafo[v1].adicionaAresta(v2))
+		if not self.grafo[v2].arestaExiste(v1): self.arestas(self.grafo[v2].adicionaAresta(v1))
 
 		return True
 
@@ -128,34 +115,10 @@ class Grafo:
 	def adjacentes(self, nome):
 		return self.grafo[nome].arestas
 
-	# Retorna uma lista dos adjacentes do vertice de um grafo
-	# Parametro nome: eh o nome do vertice que se desejam os adjacentes
-	# Parametro graph: eh o grafo que sera utilizado na busca
-	def adjacentesG(self, graph, nome):
-		return graph[nome].arestas
-
 	# Retorna o grau do vertice
 	# Parametro nome: eh o nome do vertice que se deseja o grau
 	def grau(self, nome):
 		return len(self.grafo[nome].arestas)
-
-	# Verifica se o grafo eh regular
-	def regular(self):
-		grau = -1
-		for v in self.grafo:
-			if grau < 0:
-				grau = len(self.grafo[v].arestas)
-
-			if grau != len(self.grafo[v].arestas):
-				return False
-		return True
-
-	# Verifica se o grafo eh completo
-	def completo(self):
-		for v in self.grafo:
-			if set(self.grafo[v].arestas) != set(self.grafo.keys()):
-				return False
-		return True
 
 	# Retorna o fecho transitivo de um vertice
 	# Parametro nome: eh o nome do vertice que se deseja encontrar o fecho transitivo
@@ -174,7 +137,7 @@ class Grafo:
 	# Verifica se ha algum ciclo no grafo
 	def buscaCiclo(self):
 		for v in self.grafo:
-			if self.haCicloCom(v):
+			if self._haCicloCom(v):
 				return True
 		return False
 
@@ -182,7 +145,7 @@ class Grafo:
 	# Parametro nome: eh o nome do vertice inicial
 	# Parametro vAnt (default ""): opcional, eh o vertice anterior da busca, serve para o tracking da recursao
 	# Parametro visitados (default [], lista vazia): opcional, eh uma lista de vertices que ja foram utilizados, serve para o tracking de informacoes da recursao
-	def haCicloCom(self, nome, vAnt = "", visitados = []):
+	def _haCicloCom(self, nome, vAnt = "", visitados = []):
 		if nome in visitados:
 			return True
 
@@ -204,47 +167,6 @@ class Grafo:
 			self.grafo[v].visitado = False
 			self.grafo[v].marcado = False
 
-	# Retorna uma lista com a ordem topologica do grafo
-	# Parametro limpar(default True): opcional, se verdadeiro limpa as marcacoes dos vertices (coloca visitado e marcado como falso)
-	def ordemTopologica(self, limpar = True):
-		if limpar:
-			self.limpaVertices()
-		nao_marcados = [x for x in self.grafo]
-		ordem = []
-		while nao_marcados:
-			vertice = random.choice(nao_marcados)
-			nao_marcados.remove(vertice)
-			self.visitar(vertice, ordem)
-		
-		self.limpaVertices()
-		return ordem
-
-	# Funcao auxiliar para a ordem topologica, faz a busca em profundidade e ja verifica se possui ciclo
-	# Parametro v: eh o vertice que se deseja visitar
-	# Parametro ordem: eh a lista que contem os vertices ordenados, serve para o tracking da recursao
-	def visitar(self, v, ordem):
-		if self.grafo[v].marcado:
-			return
-
-		if self.grafo[v].visitado:
-			raise ValueError("Grafo ciclico")
-
-		self.grafo[v].visitado = True
-		for vAdj in self.adjacentes(v):
-			self.visitar(vAdj, ordem)
-
-		self.grafo[v].marcado = True
-		self.grafo[v].visitado = False
-		ordem.insert(0, v)
-
-	# Funcao que busca a base de um grafo
-	# Parametro graph: opcional, eh o grafo que se deseja fazer a busca
-	def buscaBase(self, graph = grafo):
-		if self.buscaCiclo():
-			return []
-		return [v for v in graph if not self.grauEntradaG(graph, v)]
-	
-
 #######################################################################################
 # Classe vertice
 class Vertice:
@@ -252,13 +174,34 @@ class Vertice:
 	# Parametro nome eh o identificador do vertice
 	# Parametro arestas eh uma lista de conexoes do vertice (pode ser vazio)
 	# Parametro dados eh um dicionario contendo os dados que o vertice representa (pode ser vazio)
-	def __init__(self, nome, arestas, dados):
+	def __init__(self, nome, dados):
 		if nome == "":
 			raise ValueError("Nome esta em branco, por favor de um nome para o coitado")
 
 		self.nome = str(nome)
-		self.arestas = list(arestas)
+		self.arestas = list()
 		self.dados = dict(dados)
 		self.visitado = False
 		self.marcado = False
+	
+	def adicionaAresta(self, v, peso = 0):
+		a = Aresta(self, v)
+		self.arestas.append(a)
+		a.setPeso(peso)
+		return a
+	
+	def arestaExiste(self, v):
+		for a in self.arestas:
+			if a.v1 == v:
+				return True
+		return False
+#######################################################################################
+# Classe aresta
+class Aresta:
+	def __init__(self, v0, v1):
+		self.v0 = v0
+		self.v1 = v1
+	
+	def setPeso(self, valor):
+		self.peso = valor
 #######################################################################################
